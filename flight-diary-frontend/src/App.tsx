edda3cae6e6aaa3e6c72c8cd0238f8bd2237bba5
Kizuna Akari <lecion20@outlook.com>
2024-11-01 22:52:26 +0800

    9.18 post new diary

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -15,8 +15,36 @@ function App() {
     doit();
   }, [])
 
+  const diarySubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formElement = event.currentTarget
+    const formData = new FormData(formElement)
+    const obj = Object.fromEntries(formData)
+
+    console.log(obj)
+    await fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(obj)
+    })
+  }
+
   return <>
-    <h1>Ilari's flights</h1>
+    <h1>Ilari's Flights</h1>
+    <h2>New Entry</h2>
+    <form onSubmit={diarySubmit}>
+      <table>
+        <tbody>
+          <tr><td>date</td><td><input type='text' name='date' /></td></tr>
+          <tr><td>visibility</td><td><input type='text' name='visibility' /></td></tr>
+          <tr><td>weather</td><td><input type='text' name='weather' /></td></tr>
+          <tr><td>comment</td><td><input type='text' name='comment' /></td></tr>
+        </tbody>
+      </table>
+      <input type='submit' value="add" />
+    </form>
     <h2>Diary Entries</h2>
     {diaries.map(ee => (
       <div key={ee.id}>
