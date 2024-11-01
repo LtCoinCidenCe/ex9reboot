aab06ced21ceed538e4816b1fdf74b8817b5b0c3
Kizuna Akari <lecion20@outlook.com>
2024-11-01 20:43:27 +0800

    9.16 type narrowing

--- tsreact/src/components/Content.tsx
+++ tsreact/src/components/Content.tsx
@@ -1,6 +1,7 @@
-import { ModelPart, Part } from "./Part";
+import { CoursePart } from "../types";
+import { Part } from "./Part";
 
-const Content = ({ courseParts }: { courseParts: ModelPart[] }) => {
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
   return courseParts.map(part => <Part key={part.name} course={part} />)
 }
 
--- tsreact/src/components/Part.tsx
+++ tsreact/src/components/Part.tsx
@@ -1,6 +1,40 @@
-export type ModelPart = { name: string, exerciseCount: number };
-export const Part = ({ course }: { course: ModelPart }) => {
-  return <p>
-    {course.name} {course.exerciseCount}
-  </p>
+import { CoursePart } from "../types"
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+export const Part = ({ course }: { course: CoursePart }) => {
+  switch (course.kind) {
+    case "basic":
+      return <div style={{ margin: "1em 0em 1em 0em" }}>
+        <div style={{ fontWeight: "bold" }}>{course.name} {course.exerciseCount}</div>
+        <div style={{ fontStyle: "italic" }}>{course.description}</div>
+      </div>
+
+    case "group":
+      return <div style={{ margin: "1em 0em 1em 0em" }}>
+        <div style={{ fontWeight: "bold" }}>{course.name} {course.exerciseCount}</div>
+        <div>project exercises {course.groupProjectCount}</div>
+      </div>
+
+    case "background":
+      return <div style={{ margin: "1em 0em 1em 0em" }}>
+        <div style={{ fontWeight: "bold" }}>{course.name} {course.exerciseCount}</div>
+        <div style={{ fontStyle: "italic" }}>{course.description}</div>
+        <div>submit to {course.backgroundMaterial}</div>
+      </div>
+
+    case "special":
+      return <div style={{ margin: "1em 0em 1em 0em" }}>
+        <div style={{ fontWeight: "bold" }}>{course.name} {course.exerciseCount}</div>
+        <div style={{ fontStyle: "italic" }}>{course.description}</div>
+        <div>required skills: {course.requirements.join(',')}</div>
+      </div>
+    default:
+      assertNever(course);
+      break;
+  }
 }
