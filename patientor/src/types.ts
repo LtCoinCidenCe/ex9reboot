e0e03d151d820eb38de21f405f235b61df552f68
Kizuna Akari <lecion20@outlook.com>
2024-11-05 00:44:24 +0800

    9.23: Patientor, step 3

--- patientor/src/types.ts
+++ patientor/src/types.ts
@@ -1,10 +1,6 @@
 import { z } from "zod";
 import { NewPatientSchema } from "./utils";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
-}
-
 export interface Diagnosis {
   code: string,
   name: string,
@@ -25,3 +21,50 @@ export enum Gender {
   Female = "female",
   Other = "other",
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck",
+  healthCheckRating: HealthCheckRating,
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string,
+    criteria: string,
+  }
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare",
+  employerName: string,
+  sickLeave?: {
+    startDate: string,
+    endDate: string,
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, "id">;
