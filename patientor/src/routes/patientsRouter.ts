8859d91e749a0afc1bdb6ec016531192bb529f57
Kizuna Akari <lecion20@outlook.com>
2024-11-07 00:32:00 +0800

    9.27: Patientor, step 7

--- patientor/src/routes/patientsRouter.ts
+++ patientor/src/routes/patientsRouter.ts
@@ -1,8 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
-import patientService from "../services/patientService";
-import { NewPatientSchema } from "../utils";
-import { NewPatient, Patient } from "../types";
+import { v1 as uuid } from "uuid";
 import { ZodError } from "zod";
+import patientService from "../services/patientService";
+import { EntrySchema, NewPatientSchema } from "../utils";
+import { Entry, NewPatient, Patient } from "../types";
+
 
 const route = express.Router();
 
@@ -14,7 +16,8 @@ const patientValidator = (req: Request, res: Response, next: NextFunction) => {
   try {
     NewPatientSchema.parse(req.body);
     next();
-  } catch (err: unknown) {
+  }
+  catch (err: unknown) {
     if (err instanceof ZodError) {
       res.status(400).json(err).end();
     } else {
@@ -36,6 +39,53 @@ route.get("/:id", (req, res) => {
 route.post("/", patientValidator, (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
   const result = patientService.addPatient(req.body);
   res.json(result).end();
+});
+
+route.post("/:id/entries", (req, res) => {
+  const patient = patientService.getPatientbyId(req.params.id);
+  if (!patient) {
+    res.status(404).end();
+    return;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  req.body.id = "tempid";
+  try {
+    const entry = EntrySchema.parse(req.body) as Entry;
+    switch (entry.type) {
+      case "HealthCheck": {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+        const HCR = req.body.healthCheckRating;
+        if (!(typeof HCR === "number")) {
+          res.status(400).json("invalid health check rating").end();
+          return;
+        }
+        entry.healthCheckRating = HCR;
+        console.log("entry", entry);
+        const rating = Number(entry.healthCheckRating);
+        if (!(rating === 0 || rating === 1 || rating === 2 || rating === 3)) {
+          res.status(400).json("invalid health check rating").end();
+          return;
+        }
+        entry.id = uuid();
+        patient.entries.push(entry);
+        res.json(entry).end();
+      }
+        break;
+
+      default:
+        res.status(400).json("invalid entry type");
+        break;
+    }
+  }
+  catch (err: unknown) {
+    if (err instanceof ZodError) {
+      res.status(400).json(err).end();
+    }
+    else {
+      throw err;
+    }
+  }
+  res.end();
 });
 
 export default route;
--- patientor/src/utils.ts
+++ patientor/src/utils.ts
@@ -8,3 +8,12 @@ export const NewPatientSchema = z.object({
   "occupation": z.string(),
   "ssn": z.string(),
 });
+
+export const EntrySchema = z.object({
+  id: z.string(),
+  type: z.string(),
+  description: z.string(),
+  date: z.string().date(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+});
