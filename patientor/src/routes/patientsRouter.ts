714f1e59a8cb3a06ca1d411c02c522d0da84d6c5
Kizuna Akari <lecion20@outlook.com>
2024-11-08 22:40:41 +0800

    still halfwork

--- patientor/src/routes/patientsRouter.ts
+++ patientor/src/routes/patientsRouter.ts
@@ -2,9 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import { v1 as uuid } from "uuid";
 import { ZodError } from "zod";
 import patientService from "../services/patientService";
-import { EntrySchema, NewPatientSchema } from "../utils";
+import { entrySanitizer, NewPatientSchema } from "../utils";
 import { Entry, NewPatient, Patient } from "../types";
-
 
 const route = express.Router();
 
@@ -36,55 +35,37 @@ route.get("/:id", (req, res) => {
   return;
 });
 
-route.post("/", patientValidator, (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+route.post("/", patientValidator, (
+  req: Request<unknown, unknown, NewPatient>,
+  res: Response<Patient>) => {
   const result = patientService.addPatient(req.body);
   res.json(result).end();
 });
 
-route.post("/:id/entries", (req, res) => {
+const newEntryParser = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (!entrySanitizer(req.body)) {
+      res.status(400).end();
+      return;
+    }
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+route.post("/:id/entries", newEntryParser, (
+  req: Request<{ id: string }, unknown, Entry>,
+  res: Response<Entry>) => {
   const patient = patientService.getPatientbyId(req.params.id);
   if (!patient) {
     res.status(404).end();
     return;
   }
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  req.body.id = "tempid";
-  try {
-    const entry = EntrySchema.parse(req.body) as Entry;
-    switch (entry.type) {
-      case "HealthCheck": {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
-        const HCR = req.body.healthCheckRating;
-        if (!(typeof HCR === "number")) {
-          res.status(400).json("invalid health check rating").end();
-          return;
-        }
-        entry.healthCheckRating = HCR;
-        console.log("entry", entry);
-        const rating = Number(entry.healthCheckRating);
-        if (!(rating === 0 || rating === 1 || rating === 2 || rating === 3)) {
-          res.status(400).json("invalid health check rating").end();
-          return;
-        }
-        entry.id = uuid();
-        patient.entries.push(entry);
-        res.json(entry).end();
-      }
-        break;
-
-      default:
-        res.status(400).json("invalid entry type");
-        break;
-    }
-  }
-  catch (err: unknown) {
-    if (err instanceof ZodError) {
-      res.status(400).json(err).end();
-    }
-    else {
-      throw err;
-    }
-  }
+  const entry = req.body;
+  entry.id = uuid();
+  patient.entries.push(entry);
+  res.json(entry).end();
   res.end();
 });
 
--- patientor/src/utils.ts
+++ patientor/src/utils.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { Gender } from "./types";
+import { Entry, Gender, HealthCheckRating } from "./types";
 
 export const NewPatientSchema = z.object({
   "dateOfBirth": z.string().date(),
@@ -16,4 +16,56 @@ export const EntrySchema = z.object({
   date: z.string().date(),
   specialist: z.string(),
   diagnosisCodes: z.array(z.string()).optional(),
+  healthCheckRating: z.nativeEnum(HealthCheckRating).optional(),
+  discharge: z.object({
+    date: z.string().date(),
+    criteria: z.string()
+  }).optional(),
+  employerName: z.string().optional(),
+  sickLeave: z.object({
+    startDate: z.string().date(),
+    endDate: z.string().date(),
+  }).optional()
 });
+
+export const entrySanitizer = (body: unknown): body is Entry => {
+  if (typeof body !== "object" || body === null)
+    return false;
+  try {
+    const entry = EntrySchema.parse(body);
+    console.log("entry", entry);
+    switch (entry.type) {
+      case "HealthCheck": {
+        const rating = entry.healthCheckRating;
+        if (rating === undefined) {
+          return false;
+        }
+        break;
+      }
+      case "Hospital": {
+        const { discharge } = entry;
+        if (!discharge) {
+          return false;
+        }
+        break;
+      }
+      case "OccupationalHealthcare": {
+        const { employerName, sickLeave } = entry;
+        if (employerName === undefined || employerName.length === 0) {
+          return false;
+        }
+        if (!sickLeave) {
+          return false;
+        }
+        break;
+      }
+      default:
+        return false;
+        break;
+    }
+  }
+  catch {
+    return false;
+  }
+  return true;
+};
